a7cee211533dae3507374ef17f6b1b9242a5e1b4
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2017-09-10 20:52:35 +0100

    Sample ASP.NET Core 2 Web API project for RawRequestBody

---
+++ RawRequestBodySample/Controllers/RawRequestBodyController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RawRequestBodySample.Controllers
+{
+	[Route("api/[controller]")]
+	public class RawRequestBodyController : Controller
+    {
+		// GET api/values
+		[HttpGet]
+		public string Get()
+		{
+            return "Please post to api/RawRequestBody to test receiving Raw Request Body";
+		}
+
+		// POST api/values
+		[HttpPost]
+        public async Task<ContentResult> Post([FromBody] string body)
+		{
+			var requestData = body;
+
+			var response = new ContentResult
+			{
+				Content = requestData,
+				StatusCode = 200
+			};
+			return await Task.FromResult(response);
+		}
+	}
+}
---
+++ RawRequestBodySample/Formatters/RawRequestBodyInputFormatter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace RawRequestBodySample.Formatters
+{
+	public class RawRequestBodyInputFormatter : InputFormatter
+    {
+        public override Boolean CanRead(InputFormatterContext context)
+        {
+            return true;
+        }
+
+        public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context)
+        {
+			if (context == null)
+				throw new ArgumentNullException(nameof(context));
+
+			var request = context.HttpContext.Request;
+			using (var reader = new StreamReader(request.Body))
+			{
+				try
+				{
+					var content = await reader.ReadToEndAsync();
+					return await InputFormatterResult.SuccessAsync(content);
+				}
+				catch
+				{
+					return await InputFormatterResult.FailureAsync();
+				}
+			}
+		}
+    }
+}
