f52007a11448acb027a589e3090e3e63f284f303
Kashif Soofi <kashif.soofi@tandem.co.uk>
2020-01-13 16:45:37 +0000

    PingSoapService and PingWrapperSoapService

---
+++ PingSoapService/IPingService.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace PingSoapService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IPingService
+    {
+        [OperationContract]
+        PingResponse Ping(PingRequest request);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class PingRequest
+    {
+        [DataMember]
+        public bool BoolValue { get; set; }
+
+        [DataMember]
+        public string StringValue { get; set; }
+    }
+
+    [DataContract]
+    public class PingResponse
+    {
+        [DataMember]
+        public string Value { get; set; }
+    }
+}
---
+++ PingSoapService/PingService.svc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PingSoapService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class PingService : IPingService
+    {
+        public PingResponse Ping(PingRequest request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            return new PingResponse
+            {
+                Value = $"Ping Request: {request.BoolValue} - {request.StringValue}",
+            };
+        }
+    }
+}
