4a39c7b89e8ba02c922de897cb44e8d0542daa8d
Kashif Soofi <kashif.soofi+git@gmail.com>
2021-09-27 21:44:43 +0100

    JsonPath and JsonLogic examples

---
+++ JsonLogicSample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.Json;
+using Json.Logic;
+using Json.More;
+
+namespace JsonLogicSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonLogic = "{\"and\": [{\" == \": [{\"var\":\"id\"}, 1]}, {\" == \": [{\"var\":\"name\"}, \"sample\"]}]}";
+            var rule = JsonSerializer.Deserialize<Rule>(jsonLogic);
+
+            var data = new { id = 1, name = "sample" }.ToJsonDocument().RootElement;
+            var result = rule.Apply(data);
+
+            Console.WriteLine(result.ToJsonString());
+        }
+    }
+}
---
+++ JsonPathSample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Json.More;
+using Json.Path;
+
+namespace JsonPathSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var input = "$.id";
+            var input = "$.id";
+            var path = JsonPath.Parse(input);
+
+            var data = new { id = 1, name = "sample" }.ToJsonDocument().RootElement;
+            var result = path.Evaluate(data);
+
+            Console.WriteLine(result.Error);
+            Console.WriteLine(result.Matches[0].Value);
+        }
+    }
+}
