c66b4fa09f6bd4c81ccac7d95e29fadd2e4789ed
Kashif Soofi <kashif.soofi+git@gmail.com>
2021-09-27 22:14:33 +0100

    Json Schema Validation sample

---
+++ JsonSchemaSample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Json.More;
+using Json.Schema;
+
+namespace JsonSchemaSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new { id = 1, name = "sample" }.ToJsonDocument().RootElement;
+
+            var jsonSchema = "{ \"proerties\": { \"id\": { \"const\": 1 } } }";
+            var schema = JsonSchema.FromText(jsonSchema);
+            var result = schema.Validate(data);
+            Console.WriteLine(result.IsValid);
+
+            var jsonSchema1 = "{ \"properties\": { \"id\": { \"const\":1}, \"name\": { \"const\":\"sample\"} } }";
+            var schema1 = JsonSchema.FromText(jsonSchema1);
+            result = schema1.Validate(data);
+            Console.WriteLine(result.IsValid);
+        }
+    }
+}
